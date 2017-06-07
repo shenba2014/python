ad1a02dc5d497591652e67f840e8a90cce1bcc6b
luojunwen <luojunwenbashen@163.com>
2017-06-07 22:40:30 +0800

    just finish the basic knowledge

---
+++ BasicKnowledge/dataType/addNumbers.py
@@ -0,0 +1,6 @@
+def addNumbers(number):
+    return number * (number + 1) / 2
+
+print '1 =', addNumbers(1)
+print '1 + 2 + 3 =', addNumbers(3)
+print '1 + 2 + 3 .. + 100 =', addNumbers(100)
---
+++ BasicKnowledge/dataType/stringDoc.py
@@ -0,0 +1,7 @@
+class Hello:
+    '''hello class'''
+    def printHello():
+        '''print Hello method'''
+        print ("hello world!")
+print Hello.__doc__
+print Hello.printHello.__doc__
